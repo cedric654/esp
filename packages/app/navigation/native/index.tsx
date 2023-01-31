ba8b34027b9414ebfbd42dee050b2ace6d6a584d
cedric654 <gagneced22@hotmail.com>
2023-01-31 13:35:23 -0500

    Initial commit

---
+++ packages/app/navigation/native/index.tsx
@@ -0,0 +1,34 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+import { HomeScreen } from '../../features/home/screen'
+import { UserDetailScreen } from '../../features/user/detail-screen'
+
+const Stack = createNativeStackNavigator<{
+  home: undefined
+  'user-detail': {
+    id: string
+  }
+}>()
+
+export function NativeNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="user-detail"
+        component={UserDetailScreen}
+        options={{
+          title: 'User',
+          headerShown: false,
+        }}
+      />
+    </Stack.Navigator>
+  )
+}
