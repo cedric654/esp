7f3dba0d06bb96970fea9ad0b0cb696f50f173ec
cedric654 <gagneced22@hotmail.com>
2023-02-27 22:00:17 -0500

    Delete unused stuff

--- packages/app/providers/app-providers.web.tsx
+++
@@ -1,5 +0,0 @@
-import { SafeAreaProvider } from '../design/safe-area'
-
-export const AppProviders = ({ children }: { children: React.ReactNode }) => {
-  return <SafeAreaProvider>{children}</SafeAreaProvider>
-}
