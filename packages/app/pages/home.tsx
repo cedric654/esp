060f3a021c618e377df687055eba87e8eef3da85
cedric654 <gagneced22@hotmail.com>
2023-03-10 17:10:04 -0500

    Add toast and update packages

--- packages/app/pages/home.tsx
+++ packages/app/pages/home.tsx
@@ -1,12 +1,10 @@
-import { Text, View } from 'app/design'
+import { Text } from 'app/design'
 import { SafeAreaView } from 'app/design/safe-area'
-import { Spinner } from 'app/design/spinner'
 
 const HomePage = () => {
   return (
     <SafeAreaView>
       <Text tw="text-2xl">Écran accueil mobile</Text>
-      <Spinner size="large" />
     </SafeAreaView>
   )
 }
