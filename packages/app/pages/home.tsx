0bfa2a0dbb59e0bc89d2589eebea52ddfe7eec71
cedric654 <gagneced22@hotmail.com>
2023-03-10 16:39:13 -0500

    Add spinner

--- packages/app/pages/home.tsx
+++ packages/app/pages/home.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from 'app/design'
 import { SafeAreaView } from 'app/design/safe-area'
+import { Spinner } from 'app/design/spinner'
 
 const HomePage = () => {
   return (
     <SafeAreaView>
       <Text tw="text-2xl">Écran accueil mobile</Text>
+      <Spinner size="large" />
     </SafeAreaView>
   )
 }
--- packages/app/pages/home.web.tsx
+++ packages/app/pages/home.web.tsx
@@ -1,9 +1,11 @@
 import { Button, Text, View } from 'app/design'
+import { Spinner } from 'app/design/spinner'
 
 const HomePage = () => {
   return (
     <View>
       <Text tw="text-2xl">Écran accueil web</Text>
+      <Spinner size="small" />
     </View>
   )
 }
